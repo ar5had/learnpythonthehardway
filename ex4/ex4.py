2112a18a483114d4422ab3b5f140723f8dc6227e
Arshad Khan <arshdkhn1@gmail.com>
2016-08-14 01:27:02 +0530

    adding ex4

---
+++ ex4/ex4.py
@@ -0,0 +1,20 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+cars_driven = drivers
+carpool_capacity = cars_driven * space_in_a_car
+
+#There should be no underscore between 'car' and 'pool' and  it should be 'passengers' , not 'passenger'
+#average_passengers_per_car = car_pool_capacity / passenger
+
+average_passengers_per_car = passengers / cars_driven
+
+
+print "There are", cars, "cars available."
+print "There are only", drivers, "drivers available."
+print "There will be", cars_not_driven, "empty cars today."
+print "We can transport", carpool_capacity, "people today."
+print "We have", passengers, "to carpool today."
+print "We need to put about", average_passengers_per_car, "in each car."
