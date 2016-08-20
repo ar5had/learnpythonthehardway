9bd20bd33e46d40142fa8198b3f4c3138cf66824
Arshad Khan <arshdkhn1@gmail.com>
2016-08-20 22:58:57 +0530

    editing

--- ex42/ex42.py
+++ ex42/ex42.py
@@ -1,11 +1,76 @@
-class Parent(object):
-    def q(self):
-        print "This is parent method"
+## Animal is-a object (yes, sort of confusing) look at the extra credit
+class Animal(object):
+    pass
 
-class Child(Parent):
-    def p(self):
-        print "this is child method"
+## Dog is-a Animal
+class Dog(Animal):
 
-child = Child();
-child.q()
-child.p()
+    def __init__(self, name):
+        ## Dog has-a name property or variable
+        self.name = name
+
+## Cat is-a Animal
+class Cat(Animal):
+
+    def __init__(self, name):
+        ## Cat has-a name property
+        self.name = name
+
+## Person is-a object
+class Person(object):
+
+    def __init__(self, name):
+        ## Person has-a name property
+        self.name = name
+
+        ## Person has-a pet of some kind
+        self.pet = None
+
+## Employee is-a Person
+class Employee(Person):
+
+    def __init__(self, name, salary):
+        ## ?? hmm what is this strange magic?
+        super(Employee, self).__init__(name)
+        ## Employee has-a salary property
+        self.salary = salary
+
+## Fish is-a object
+class Fish(object):
+    pass
+
+## Salmon is-a Fish
+class Salmon(Fish):
+    pass
+
+## Halibut is-a Fish
+class Halibut(Fish):
+    pass
+
+
+## rover is-a Dog
+rover = Dog("Rover")
+
+## satan is-a Cat
+satan = Cat("Satan")
+
+## mary is-a Person
+mary = Person("Mary")
+
+## mary has-a pet
+mary.pet = satan
+
+## frank is-a Employee
+frank = Employee("Frank", 120000)
+
+## frank has-a pet
+frank.pet = rover
+
+## flipper is-a Fish
+flipper = Fish()
+
+## crouse is-a Salmon
+crouse = Salmon()
+
+## harry is-a Halibut
+harry = Halibut()
