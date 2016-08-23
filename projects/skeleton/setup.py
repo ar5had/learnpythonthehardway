724e6280859145f399e1d4227f04d9b11df7e343
Arshad Khan <arshdkhn1@gmail.com>
2016-08-23 20:46:10 +0530

    editing

---
+++ projects/skeleton/setup.py
@@ -0,0 +1,20 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Arshad Khan',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'arshdkhn1@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
+
